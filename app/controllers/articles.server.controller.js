094c4ad0a38b664d0e8062993c7a12a1c24d3f7a
Kris <mentheosis@gmail.com>
2014-12-15 01:35:54 -0500

    fix some release mistakes

--- app/controllers/articles.server.controller.js
+++ app/controllers/articles.server.controller.js
@@ -40,9 +40,8 @@ exports.read = function(req, res) {
 exports.update = function(req, res) {
 	var article = req.article;
 
-	console.log('resetting parent');
-	//article.parent = '';//article.id;
-
+console.log('resetting parent');
+	article.parent = '';//article.id;
 
 	article = _.extend(article, req.body);
 
@@ -58,43 +57,26 @@ exports.update = function(req, res) {
 };
 
 exports.kismet = function(req, res) {
+	var json_rpc_req = req.body;
+	var method = json_rpc_req.method;
+	var params = json_rpc_req.params;
 
-	var article = req.article;
-
-	article = _.extend(article, req.body);
-
-	//article.kismet ++;
-
-	article.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
+	switch(method) {
+	case 'SEND':
+		Article.findOneAndUpdate(
+			{id: req.article.id},
+			{$inc: {'articles.kismet': params.amt}},
+			{upsert: true},
+			function(err, article) {
+				if(err) { return res.status(400).send({ message: errorHandler.getErrorMessage(err) }); }
+				else {
+					res.json(article);
+				}
 			});
-		} else {
-			res.json(article);
-		}
-	});
-
-//	console.log(req.locals.tx);
-//	if(req.locals.tx)
-//	{
-//		var update = { $inc: { views: 1 }};
-//		Article.update(
-//			{id : req.article._id },
-//			{ $inc: { 'articles.kismet': 1 } },
-//			function(err, article) {
-//				if(err) {
-//					console.log('articles.server.controller.kismet: ERROR incrementing KSMT for article_id: ' + req.article.id);
-//					return res.status(400).send({ message: errorHandler.getErrorMessage(err) });
-//				}
-//				else {
-//					console.log('articles.server.controller.kismet: incrementing KSMT for article_id: ' + req.article.id);
-//					res.json(article);
-//				}
-//			}
-//		);
-//	}
-
+		break;
+	default:
+		break;
+	}
 };
 
 /**
@@ -118,7 +100,7 @@ exports.delete = function(req, res) {
  * List of Articles
  */
 exports.list = function(req, res) {
-	Article.find({parent:'top'}).sort('-created').populate('user', 'displayName').exec(function(err, articles) {
+	Article.find({parent:'top'}).sort('-created').populate('user', 'username').exec(function(err, articles) {
 		if (err) {
 			return res.status(400).send({
 				message: errorHandler.getErrorMessage(err)
@@ -133,7 +115,7 @@ exports.listComments = function(req, res) {
 //	console.log('list:');
 //	console.log(JSON.stringify(req.query));
 
-	Article.find({parent:req.param('parentId')}).sort('-created').populate('user', 'displayName').exec(function(err, articles) {
+	Article.find({parent:req.param('parentId')}).sort('-created').populate('user', 'username').exec(function(err, articles) {
 		if (err) {
 			return res.status(400).send({
 				message: errorHandler.getErrorMessage(err)
